e2572e02197a86c398522d2127041195847324e4
rjhear <72757968+rjhear@users.noreply.github.com>
2022-12-01 18:53:55 -0500

    Add R solution

---
+++ 01.R
@@ -0,0 +1,50 @@
+#' 01.R
+#' Day 1: Calorie Counting
+#' https://adventofcode.com/2022/day/1
+#' 
+#'  ========= SOLUTIONS ==========
+#' + PART 1 .......65,912 calories
+#' + PART 2 ......195,625 calories
+
+packages <- c("tidyverse")
+lapply(packages, require, character.only=T)
+
+repo.local <- "~/repos/_misc/adventofcode"
+puzzle.input <- "./data/01_input.txt"
+
+setwd(repo.local)
+
+get_cal_totals <- function(filepath) {
+  calories <- readLines(filepath)
+  n.vec <- length(calories)
+  breaks <- which(grepl("^[[:space:]]*$", calories))
+  n.breaks <- length(breaks)
+  if (breaks[n.breaks] < n.vec) {
+    breaks <- c(breaks, n.vec + 1L)
+    n.breaks <- n.breaks + 1L
+  }
+  if (n.breaks > 0L) {
+    calories <-
+      mapply(function(i, j) {
+        paste(calories[i:j], collapse = " ")
+      },
+      c(1L, 1L + breaks[-n.breaks]), breaks - 1L)
+  }
+  calories <- lapply(calories, function(x){as.numeric(strsplit(x, split=" ")[[1]])})
+  calories <- sapply(calories, sum)
+  return (calories)
+}
+
+get_max_cals <- function(total_calories) {
+  return (max(unlist(total_calories)))
+}
+
+sum_top_n_max_cals <- function(total_calories) {
+  total_calories <- sort.int(total_calories)
+  return(sum(total_calories[seq.int(to=length(total_calories), length.out=3)]))
+}
+
+calories <- get_cal_totals(puzzle.input)
+print(get_max_cals(calories))
+print(sum(tail(sort.int(calories), 3)))
+print(sum_top_n_max_cals(calories))
