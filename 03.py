08a814a133ceee7cc10d75e1d90fa042d3273f0b
rjhear <72757968+rjhear@users.noreply.github.com>
2022-12-03 18:08:34 -0500

    Use map instead of comprehension

--- 03.py
+++ 03.py
@@ -13,41 +13,51 @@ from typing import Generator
 
 
 def main(game_input: str) -> None:
-    print(f"""{f" SOLUTIONS ":=^35}""")
-    print(f"""+ PART 1: {run_part_1(game_input):.>12,} priority sum""")
-    print(f"""+ PART 2: {run_part_2(game_input):.>12,} priority sum""")
+    print_solutions(game_input=game_input, part_1=run_part_1, part_2=run_part_2)
 
 
-def _get_inputs(game_inputs: str) -> list: return Path(game_inputs).read_text().splitlines()
+def _get_inputs(game_input: str) -> list: return Path(game_input).read_text().splitlines()
 
 
 def find_common_in_rucksack(items: str) -> set:
-    """"""
+    """Get the common items in both compartments"""
     return set(items[:(compartment := len(items) >> 1)]) & set(items[compartment:])
 
 
-def find_common_among_rucksacks(rucksacks: list) -> Generator:
-    """"""
+def find_common_among_rucksacks(rucksacks: Generator) -> Generator:
+    """Get the common items among n rucksacks"""
     return ((reduce(and_, map(set, group))) for group in rucksacks)
 
 
 def get_priority(dupes: set) -> int:
-    """"""
+    """Map the letter to its priority value"""
     # {chr(i): ord(chr(i)) for i in range(65, 123) if i not in range(91, 97)}
     return next((ascii_code := ord(dupe)) - 96 + 58 * (1 - ascii_code // 91) for dupe in dupes)
 
 
 def run_part_1(game_input: str) -> int:
     """"""
-    return sum(get_priority(find_common_in_rucksack(items)) for items in _get_inputs(game_input))
+    return sum(map(get_priority, map(find_common_in_rucksack, _get_inputs(game_input))))
 
 
 def run_part_2(game_input: str, grouping: int = 3) -> int:
     """"""
-    rucksacks = _get_inputs(game_input)
+    rucksacks: list = _get_inputs(game_input)
     grouped = (rucksacks[n:n + grouping] for n in range(0, len(rucksacks), grouping))
-    common = find_common_among_rucksacks(grouped)
-    return sum(get_priority(badge) for badge in common)
+    common: Generator = find_common_among_rucksacks(grouped)
+    return sum(map(get_priority, common))
+
+
+def print_solutions(*, game_input: str, part_1: callable = None, part_2: callable = None) -> None:
+    """"""
+    if not any((part_1, part_2)): raise ValueError("At least one function must be provided")
+    print(f"""{f" SOLUTIONS ":=^35}""")
+    if all((part_1, part_2)):
+        for i, solution in enumerate((part_1, part_2), 1):
+            print(f"""+ PART {i}: {solution(game_input):.>12,} priority sum""")
+    else:
+        i, solution = 1 if part_1 else 2, part_1 or part_2
+        print(f"""+ PART {i}: {solution(game_input):.>12,} priority sum""")
 
 
 if __name__ == "__main__":
