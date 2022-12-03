568f0831b317d0a114d3d21236695482b880620a
rjhear <72757968+rjhear@users.noreply.github.com>
2022-12-03 14:56:30 -0500

    Finish day 3

--- 03.py
+++ 03.py
@@ -4,23 +4,31 @@ https://adventofcode.com/2022/day/2
 
 ============ SOLUTIONS ============
 + PART 1: .......7,917 priority sum
-
++ PART 2: .......2,585 priority sum
 """
+from functools import reduce
+from operator import and_
 from pathlib import Path
+from typing import Generator
 
 
 def main(game_input) -> None:
     print(f"""{f" SOLUTIONS ":=^35}""")
     print(f"""+ PART 1: {run_part_1(game_input):.>12,} priority sum""")
-
-    
-def _get_inputs(game_inputs):
-    return Path(game_inputs).read_text().splitlines()
+    print(f"""+ PART 2: {run_part_2(game_input):.>12,} priority sum""")
 
 
-def get_intersection(string: str) -> set:
+def _get_inputs(game_inputs): return Path(game_inputs).read_text().splitlines()
+
+
+def find_common_in_rucksack(string: str) -> set:
     """"""
     return set(string[:(split := len(string) >> 1)]) & set(string[split:])
+
+
+def find_common_in_rucksacks(rucksacks: list) -> Generator:
+    """"""
+    return ((reduce(and_, map(set, group))) for group in rucksacks)
 
 
 def get_priority(dupes: set) -> int:
@@ -30,11 +38,14 @@ def get_priority(dupes: set) -> int:
 
 
 def run_part_1(game_input: str) -> int:
-    return sum(get_priority(get_intersection(items)) for items in _get_inputs(game_input))
+    return sum(get_priority(find_common_in_rucksack(items)) for items in _get_inputs(game_input))
 
 
-def run_part_2():
-    ...
+def run_part_2(game_input: str, grouping: int = 3) -> int:
+    rucksacks = _get_inputs(game_input)
+    grouped = [rucksacks[n:n + grouping] for n in range(0, len(rucksacks), grouping)]
+    common = find_common_in_rucksacks(grouped)
+    return sum(get_priority(badge) for badge in common)
 
 
 if __name__ == "__main__":
