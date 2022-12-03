e8075b939909a51d2554468308c771b2ae60f47b
rjhear <72757968+rjhear@users.noreply.github.com>
2022-12-03 15:06:35 -0500

    Add typing

--- 03.py
+++ 03.py
@@ -18,12 +18,12 @@ def main(game_input) -> None:
     print(f"""+ PART 2: {run_part_2(game_input):.>12,} priority sum""")
 
 
-def _get_inputs(game_inputs): return Path(game_inputs).read_text().splitlines()
+def _get_inputs(game_inputs) -> list: return Path(game_inputs).read_text().splitlines()
 
 
-def find_common_in_rucksack(string: str) -> set:
+def find_common_in_rucksack(items: str) -> set:
     """"""
-    return set(string[:(split := len(string) >> 1)]) & set(string[split:])
+    return set(items[:(compartment := len(items) >> 1)]) & set(items[compartment:])
 
 
 def find_common_among_rucksacks(rucksacks: list) -> Generator:
@@ -33,17 +33,19 @@ def find_common_among_rucksacks(rucksacks: list) -> Generator:
 
 def get_priority(dupes: set) -> int:
     """"""
-    # print({chr(i): ord(chr(i)) for i in range(65, 123) if i not in range(91, 97)})
+    # {chr(i): ord(chr(i)) for i in range(65, 123) if i not in range(91, 97)}
     return next((ascii_code := ord(dupe)) - 96 + 58 * (1 - ascii_code // 91) for dupe in dupes)
 
 
 def run_part_1(game_input: str) -> int:
+    """"""
     return sum(get_priority(find_common_in_rucksack(items)) for items in _get_inputs(game_input))
 
 
 def run_part_2(game_input: str, grouping: int = 3) -> int:
+    """"""
     rucksacks = _get_inputs(game_input)
-    grouped = [rucksacks[n:n + grouping] for n in range(0, len(rucksacks), grouping)]
+    grouped = (rucksacks[n:n + grouping] for n in range(0, len(rucksacks), grouping))
     common = find_common_among_rucksacks(grouped)
     return sum(get_priority(badge) for badge in common)
 
