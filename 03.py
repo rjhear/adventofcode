0915a88e78cd42240a6bd1309cdf8be719f1593c
rjhear <72757968+rjhear@users.noreply.github.com>
2022-12-02 21:49:02 -0500

    Add day 3 template

---
+++ 03.py
@@ -0,0 +1,19 @@
+"""03.py
+Day 3
+https://adventofcode.com/2022/day/2
+
+
+"""
+from pathlib import Path
+
+
+def main():
+    ...
+
+
+def _get_inputs(game_inputs):
+    ...
+
+
+if __name__ == "__main__":
+    main()
