63fc20fdb5f452080fc771d729a8858c239f0da8
rjhear <72757968+rjhear@users.noreply.github.com>
2022-12-04 23:40:57 -0500

    Finish day 4

---
+++ 04.py
@@ -0,0 +1,48 @@
+"""04.py
+Day 4: Camp Cleanup
+https://adventofcode.com/2022/day/4
+
+=============== SOLUTIONS ================
++ PART 1: .............................530
++ PART 2: .............................903
+"""
+from functools import partial
+from itertools import chain, starmap
+
+from utils.helpers import _get_inputs, performance_timer, print_solutions
+
+
+@performance_timer
+def main(game_input: str) -> None:
+    part_2 = partial(find_assignment_overlaps, fully_contains=False)
+    print_solutions(game_input=game_input, part_1=find_assignment_overlaps, part_2=part_2)
+
+
+def find_assignment_overlaps(game_input: str, fully_contains: bool = True) -> int:
+    """"""
+    overlaps = 0
+    pairs = (chain(*(pair.splitlines() for pair in _get_inputs(game_input))))
+    for assignment in pairs:
+        check_overlap = (check_is_overlap, check_is_super_or_sub_set)[fully_contains]
+        overlap = check_overlap(*(tuple(map(int, elf.split("-"))) for elf in assignment.split(",")))
+        if overlap: overlaps += 1
+    return overlaps
+
+
+def check_is_super_or_sub_set(range1: list, range2: list) -> bool:
+    """"""
+    start, stop = 0, 1
+    r1_range, r2_range = starmap(range, ((range1[start], range1[stop] + 1), (range2[start], range2[stop] + 1)))
+    return set(r1_range).issubset(r2_range) or set(r1_range).issuperset(r2_range)
+
+
+def check_is_overlap(range1: list, range2: list) -> bool:
+    """"""
+    start, stop = 0, -1
+    r1_range, r2_range = starmap(range, ((range1[start], range1[stop] + 1), (range2[start], range2[stop] + 1)))
+    return bool(list(range(max(r1_range[0], r2_range[0]), min(r1_range[-1], r2_range[-1]) + 1)))
+
+
+if __name__ == "__main__":
+    puzzle_input = r"./data/04_input.txt"
+    main(game_input=puzzle_input)
