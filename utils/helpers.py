0330b1058330e4d124943dae7c3623c5d8361bf3
rjhear <72757968+rjhear@users.noreply.github.com>
2022-12-05 23:45:27 -0500

    Force print to work with different types

--- utils/helpers.py
+++ utils/helpers.py
@@ -10,14 +10,24 @@ def print_solutions(*, game_input: str, part_1: callable = None, part_2: callabl
     """"""
     if not any((part_1, part_2)): raise ValueError("At least one function must be provided")
     print((header := f"""{f" SOLUTIONS ":=^42}"""))
-    if all((part_1, part_2)):
-        for i, solution in enumerate((part_1, part_2), 1):
+    try:
+        if all((part_1, part_2)):
+            for i, solution in enumerate((part_1, part_2), 1):
+                message = f"""+ PART {i}: {solution(game_input):.>12,}"""
+                print(f"""+ PART {i}: {solution(game_input):.>{12 + len(header) - len(message)},} {descrip}""")
+        else:
+            i, solution = (1, 2)[part_1 is None], part_1 or part_2
             message = f"""+ PART {i}: {solution(game_input):.>12,}"""
             print(f"""+ PART {i}: {solution(game_input):.>{12 + len(header) - len(message)},} {descrip}""")
-    else:
-        i, solution = (1, 2)[part_1 is None], part_1 or part_2
-        message = f"""+ PART {i}: {solution(game_input):.>12,}"""
-        print(f"""+ PART {i}: {solution(game_input):.>{12 + len(header) - len(message)},} {descrip}""")
+    except ValueError:
+        if all((part_1, part_2)):
+            for i, solution in enumerate((part_1, part_2), 1):
+                message = f"""+ PART {i}: {solution(game_input):.>12}"""
+                print(f"""+ PART {i}: {solution(game_input):.>{12 + len(header) - len(message)}} {descrip}""")
+        else:
+            i, solution = (1, 2)[part_1 is None], part_1 or part_2
+            message = f"""+ PART {i}: {solution(game_input):.>12}"""
+            print(f"""+ PART {i}: {solution(game_input):.>{12 + len(header) - len(message)}} {descrip}""")
 
 
 def performance_timer(func):
@@ -30,7 +40,7 @@ def performance_timer(func):
         stop = perf_counter_ns()
         print(f"\n{' PERFORMANCE ':=^42}")
         print(f"+ FUNCTION: {func.__name__!r} used")
-        print(f"""{ f' {str((stop - start) / 1_000_000_000)}':>>34} SECONDS""")
+        print(f"""{f' {str((stop - start) / 1_000_000_000)}':>>34} SECONDS""")
         print(f"START: {begin:.>35}")
         print(f"STOP: {datetime.now().strftime('%Y-%m-%d %H:%M:%S:%f'):.>36}")
         return result
