d2353a962532a8bb7e190be72f0c5b47e8ecf674
rjhear <72757968+rjhear@users.noreply.github.com>
2022-12-08 00:17:58 -0500

    Add day 8

---
+++ 08.py
@@ -0,0 +1,15 @@
+"""08.py
+Day 8: Treetop Tree House
+https://adventofcode.com/2022/day/8
+
+"""
+from utils.helpers import _get_inputs, performance_timer, print_solutions
+
+
+def main(game_input: str) -> None:
+    ...
+
+
+if __name__ == "__main__":
+    puzzle_input = r"./data/08_input.txt"
+    main(puzzle_input)
