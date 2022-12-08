93d97ea8105538477e65609482d5cd8a9aa739e3
rjhear <72757968+rjhear@users.noreply.github.com>
2022-12-07 22:41:21 -0500

    Add 6 & 7

---
+++ 06.py
@@ -0,0 +1,15 @@
+"""06.py
+Day 6: Tuning Trouble
+https://adventofcode.com/2022/day/6
+
+"""
+from utils.helpers import _get_inputs, performance_timer, print_solutions
+
+
+def main(game_input: str) -> None:
+    ...
+
+
+if __name__ == "__main__":
+    puzzle_input = r"./data/06_input.txt"
+    main(puzzle_input)
